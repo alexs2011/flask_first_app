227fe8e61e23b8ad07dd81f3d8b987eccacc62bd
Alexander <kir-al93@yandex.ru>
2022-04-14 14:08:41 +0900

    Fixed a typo

--- utility/utils.py
+++ utility/utils.py
@@ -3,7 +3,7 @@ import json
 
 def load_data(filename: str) -> list[dict]:
     """
-    Загружает данные их файла формата JSON.
+    Загружает данные из файла формата JSON.
     """
     with open(filename, 'r', encoding='utf-8') as f_in:
         return json.load(f_in)
