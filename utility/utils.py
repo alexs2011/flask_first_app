022f32b4d4de06ed427531a607388e7a51faafb7
Alexander <kir-al93@yandex.ru>
2022-04-14 13:13:14 +0900

    Added search_by_skill.html template

--- utility/utils.py
+++ utility/utils.py
@@ -10,7 +10,22 @@ def load_data(filename: str) -> list[dict]:
 
 
 def find_candidate_by_uid(candidates: list[dict], uid: int) -> dict:
+    """
+    Поиск кандидата по uid.
+    """
     for candidate in candidates:
         if candidate.get("id") == uid:
             return candidate
     raise ValueError(f"ValueError: Кандидат с uid={uid} не найден.")
+
+
+def find_candidates_by_skill(candidates: list[dict], skill: str) -> list[dict]:
+    """
+    Поиск всех кандидатов, у которых есть навык skill.
+    """
+    res = []
+    for candidate in candidates:
+        skills = candidate.get("skills").lower().split(", ")
+        if skill.lower() in skills:
+            res.append(candidate)
+    return res
--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 from flask import render_template, abort
 
 from app import app, candidates
-from utility.utils import find_candidate_by_uid
+from utility.utils import find_candidate_by_uid, find_candidates_by_skill
 
 
 @app.route('/')
@@ -19,7 +19,16 @@ def candidate_profile(uid: int) -> str:
     """
     try:
         candidate = find_candidate_by_uid(candidates, uid)
-    except ValueError as e:
+    except ValueError as e:  # Если нет кандидата с данным uid.
         print(e)
         abort(404)
     return render_template("candidate.html", candidate=candidate)
+
+
+@app.route('/skills/<skill>')
+def search_by_skill(skill: str) -> str:
+    """
+    Страница с информацией о тех кандидатах, у которых есть навык skill.
+    """
+    filtered_candidates = find_candidates_by_skill(candidates, skill)
+    return render_template("search_by_skill.html", candidates=filtered_candidates, skill=skill)
